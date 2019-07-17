0ef732246c9f918f2b587103c4c90efb89c3d352
Mathias Monstrey <me@mathiasmonstrey.be>
2019-07-17 15:11:37 +0200

    adding the codebase

---
+++ route-generation.ts
@@ -0,0 +1,36 @@
+import VueRouter, { RouteConfig } from 'vue-router';
+import Vue, { VueConstructor } from 'vue';
+
+export default {
+
+
+    install(vue: typeof Vue, { router, error }: { router: VueRouter, error: VueConstructor<Vue> }): void {
+        router.addRoutes(this.getAllRoutes(error));
+    },
+
+    getModuleRoutes(): RouteConfig[] {
+        const routeFiles = require.context('@/app', true, /[A-Za-z0-9-_]+\-routes\.ts$/i);
+        const routes: RouteConfig[] = [];
+        routeFiles.keys().forEach((key) => {
+            routeFiles(key).default.forEach((route: RouteConfig) => {
+                routes.push(route);
+            });
+        });
+        return routes;
+    },
+
+    getIgnoredRoutes(error: VueConstructor<Vue>): RouteConfig[] {
+        if (error) {
+            return [
+                { path: '/404', component: error },
+                { path: '/403', component: error },
+                { path: '/*', redirect: '/404' },
+            ];
+        }
+        return [];
+    },
+
+    getAllRoutes(error: VueConstructor<Vue>): RouteConfig[] {
+        return this.getModuleRoutes().concat(this.getIgnoredRoutes(error));
+    }
+};
